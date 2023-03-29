84c59b7f0af5fed5e13cf1826a05137c99d1f08b
dasha <dasha@gmail.ru>
2023-03-29 13:15:02 +0300

    papapka

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Привет дядя Дима!");
+Console.WriteLine("Работа на оценку 3");
+
+int[] array = { 1, 2, 3, 4, 5 };
+int max = array.Max();
+int min = array.Min();
+int sum = array.Sum();
+double avg = array.Average();
+int product = 1;
+for (int i = 0; i < array.Length; i++)
+{
+    product *= array[i];
+}
+
+Console.WriteLine($"Max: {max}");
+Console.WriteLine($"Min: {min}");
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Avg: {avg}");
+Console.WriteLine($"Mul: {product}");
+Console.ReadKey();
+Console.WriteLine();
+  
